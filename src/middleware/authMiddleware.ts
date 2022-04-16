b65c353611501a94f021787dc9716c8f24e76e10
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-04-16 11:24:02 +0100

    authGuard middleware

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,38 @@
+import expressAsyncHandler from "express-async-handler"
+import jwt from "jsonwebtoken"
+import UserModel from "../models/UserModel"
+
+interface JwtPayload {
+  id: string
+}
+
+const authGuard = expressAsyncHandler(
+  async (req: any, res, next): Promise<void> => {
+    let token
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      try {
+        token = req.headers.authorization.split(" ")[1]
+
+        const { id } = jwt.verify(token, "Secret") as JwtPayload
+
+        req.user = await UserModel.findById(id).select("-password")
+
+        next()
+      } catch (error) {
+        console.error(error)
+        res.status(401)
+        throw new Error("Not authorized, token failed")
+      }
+    }
+
+    if (!token) {
+      res.status(401)
+      throw new Error("Unauthorized")
+    }
+  }
+)
+
+export { authGuard }
