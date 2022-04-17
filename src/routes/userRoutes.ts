b1d40358f38b817b43c20e0993ca8424e75ef761
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-04-17 21:26:13 +0100

    user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,20 @@
-import express from 'express'
-import { authGuard } from '../middleware/authMiddleware'
-import { authenticateUserService, deleteUserProfileService, getUserProfileService, registerUserService, updateUserProfileService } from '../services/userService'
+import express from "express"
+import { authenticateUserController, deleteUserProfileController, getUserProfileController, registerUserController, resetUserDepositController, updateUserProfileController, userAccountDepositController } from "../controllers/userController"
+import { authGuard } from "../middleware/authMiddleware"
+
 
 const router = express.Router()
 
-router.post('/register', registerUserService)
-router.post('/login', authenticateUserService)
-router.route('/profile').get(authGuard, getUserProfileService).put(authGuard, updateUserProfileService)
-router.delete('/profile/delete', authGuard, deleteUserProfileService)
+router.post("/register", registerUserController)
+router.post("/login", authenticateUserController)
+router
+  .route("/profile")
+  .get(authGuard, getUserProfileController)
+  .put(authGuard, updateUserProfileController)
+router.delete("/profile/delete", authGuard, deleteUserProfileController)
+
+//Deposit
+router.put("/deposit", authGuard, userAccountDepositController)
+router.put("/deposit/reset", authGuard, resetUserDepositController)
 
 export default router
