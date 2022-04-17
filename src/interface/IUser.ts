0f2c70c43bf4ed714d515baaf961b70a1605f2ab
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-04-17 21:26:37 +0100

    user interface

--- src/interface/IUser.ts
+++ src/interface/IUser.ts
@@ -7,3 +7,10 @@ export interface IUserLogin {
   username: string;
   password: string;
 }
+
+export interface IUserUpdate {
+  _id: string
+  deposit?: number
+  username?: string
+  role?: string
+}
